8ec546f4e53a15e7852037cf01f870bfcd3f320d
Adrian Fernando Rodríguez Garay <159966093+A23110197@users.noreply.github.com>
2024-04-23 00:20:08 +0000

    Version 5

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,34 @@
 #include<ftxui/screen/string.hpp>
 #include<ftxui/screen/terminal.hpp>
 #include <fstream>
+#include <Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    fstream archivo;
-    archivo.open("./assests/images/canon.txt");
-
-    string canon;
-    archivo >> canon;
-    archivo.close();
-
-    archivo.open("./assests/images/alien");
-    string alien;
-    archivo >> alien;
-    archivo.close();
+    Dibujo dtanque("./assets/images/canon.txt");
+    Dibujo dalien("./assets/images/alien.txt");
+    Dibujo dbloque("./assets/images/canon.txt");
 
     int fotograma = 0;
     while(true)
     {
-    fotograma++;
-    Element personaje = spinner(21, fotograma) | bold | color(Color::Yellow) | bgcolor(Color::Green1);
-    Element tanque = text(canon) | bold | color(Color::Green) | bgcolor(Color::Blue);
-    Element lienzo = hbox((personaje, tanque));
+        fotograma++;
+        Element personaje = spinner(21, fotograma) | bold | color(Color::Yellow) | bgcolor(Color::Green1);
+        Element tanque = dtanque.GetElement() | bold | color(Color::Green) | bgcolor(Color::Blue);
+        Element lienzo = hbox((personaje, tanque));
 
-    Screen pantalla = Screen::Create(
-        Dimension::Full(),
-        Dimension::Fit(lienzo));
+        Screen pantalla = Screen::Create(
+            Dimension::Full(),
+            Dimension::Fit(lienzo));
 
-    Render(pantalla, lienzo);
-    pantalla.Print();
-    cout<<pantalla.ResetPosition();
+        Render(pantalla, lienzo);
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
 
-    this_thread::sleep_for(0.1s);
-
+        this_thread::sleep_for(0.1s);
     }
-
     return 0;
 }
